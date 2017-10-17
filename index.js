84e36d6abd5dfe38c0eadb97ac213bf332e00e49
chaintng <chaintng@gmail.com>
2017-10-17 16:32:44 +0700

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const line = require('@line/bot-sdk')
+require('dotenv').config()
+
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
+const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+
+app.listen(3000);
