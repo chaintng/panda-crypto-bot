133b1f6394a409f930739109b5fb2c64cb949738
chaintng <chaintng@gmail.com>
2017-10-17 17:47:28 +0700

    add bitcoin price fetch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const request = require('request-promise')
 const line = require('@line/bot-sdk')
 require('dotenv').config()
 
@@ -28,11 +29,19 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  if (["btc", "bitcoin"].indexOf(event.message.text) >= 0) {
-    return client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: "ราคา เท่านี้"
-    });
+  if (["btc", "bitcoin"].indexOf(event.message.text.toLowerCase()) >= 0) {
+    return request("https://bx.in.th/api/")
+      .then((string) => {
+        const json = JSON.parse(string)
+        const lastBtcThbprice = json["1"].last_price
+        const change = json["1"].change
+        const text = `ราคา 1 Bitcoin ตอนนี้เท่ากับ ${lastBtcThbprice}, เปลี่ยนแปลง ${change}%`
+
+        return client.replyMessage(event.replyToken, {
+          type: 'text',
+          text: text
+        });
+      })
   }
 
   return Promise.resolve(null);
