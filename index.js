58ff4f8df091cc1a673506626b9667b983ef1de9
chaintng <chaintng@gmail.com>
2017-10-17 17:41:29 +0700

    add keyword

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ require('dotenv').config()
 
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+  verify: false
 };
 
 const app = express();
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
+    .then((result) => {
+      console.log(result)
+      return res.json(result)
+    })
+    .catch((e) => {
+      console.log(e)
+      return res.send("Error")
+    })
 });
 
 const client = new line.Client(config);
@@ -20,10 +28,14 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  });
+  if (["btc", "bitcoin"].indexOf(event.message.text) >= 0) {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: "ราคา เท่านี้"
+    });
+  }
+
+  return Promise.resolve(null);
 }
 
 app.listen(process.env.PORT || 3000);
